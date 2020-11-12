13a9eeec80ee5364d19ab8d4953e391817b53e98
Bruno Laviso <bruno.laviso@hotmail.com>
2020-11-11 22:58:51 -0300

    Add método de exibicao das imagens Original e Processada

--- src/main/java/www/Controller/ImageController.java
+++ src/main/java/www/Controller/ImageController.java
@@ -5,10 +5,26 @@
  */
 package www.Controller;
 
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Bruno Laviso
  */
 public class ImageController {
+
+    public void exibeImagemProcessada(ImageProcessor saida, JLabel label) {
+        ImagePlus iplus = new ImagePlus(null, saida);
+        
+        double scale = (double)label.getHeight() / iplus.getProcessor().getHeight();
+        int sizeW = (int) (iplus.getProcessor().getWidth() * scale);
+        ImagePlus iplusThumbnail = new ImagePlus(null, iplus.getProcessor().resize(sizeW, label.getHeight()));
+        
+        label.setText(null);
+        label.setIcon(new ImageIcon(iplusThumbnail.getBufferedImage()));
+    }
     
 }
