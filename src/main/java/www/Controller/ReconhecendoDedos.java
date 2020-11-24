f0243d07d676e8824c70ace934b913ab715e9be7
Bruno Laviso <bruno.laviso@hotmail.com>
2020-11-24 17:31:42 -0300

    Vetores alimentados com erro do maven

--- src/main/java/www/Controller/ReconhecendoDedos.java
+++ src/main/java/www/Controller/ReconhecendoDedos.java
@@ -1,21 +1,45 @@
 package www.Controller;
 
 import ij.ImagePlus;
+import ij.plugin.filter.GaussianBlur;
+import ij.process.ImageProcessor;
 import java.io.File;
 
 public class ReconhecendoDedos {
 
-    // 1 ArrayList 60 posicoes
-    // foreach preenchendo 30 primeiras com 1 dedo
-    // foreach preenchendo 30 ultimas com 3 dedos
     public static void main(String[] args) {
-        ImagePlus Imagemteste;
-        String[] ArrayDedos = new String[50];
+        String[] PathImgs = new String[61];
+        double[] meansImgs = new double[61];
+        ImagePlus img;
+        ImageProcessor ip;
+        GaussianBlur gb = new GaussianBlur();
+
         for (int i = 1; i <= 30; i++) {
-            ArrayDedos[i] = new File(String.format("assets/1/1 dedo/d - %d.png", i)).getPath();//ImageIO.read(new File();
-            System.out.println(ArrayDedos[i]);
-        //Imagemteste = ij.IJ.openImage(ArrayDedos[i]);         - Teste para leitura
-        //Imagemteste.show();                                   - Teste para exibição das imagens EM LOTE!
+            PathImgs[i] = new File(String.format("assets/1/1 dedo/d - %d.png", i)).getPath();//ImageIO.read(new File();
+            System.out.println(PathImgs[i]);
+            img = ij.IJ.openImage(PathImgs[i]); // Teste para leitura
+            //Imagemteste.show(); // Teste para exibição das imagens EM LOTE!
+
+            ip = img.getProcessor();
+            gb.blur(ip, 5);
+            ip.autoThreshold();
+
+            meansImgs[i] = ip.getStatistics().mean;
+            System.out.println(meansImgs[i]);
+        }
+        
+        for (int i = 1; i <= 30; i++) {
+            PathImgs[i] = new File(String.format("assets/3/3 dedos/d - %d.png", i)).getPath();//ImageIO.read(new File();
+            System.out.println(PathImgs[i]);
+            img = ij.IJ.openImage(PathImgs[i]); // Teste para leitura
+            //Imagemteste.show(); // Teste para exibição das imagens EM LOTE!
+
+            ip = img.getProcessor();
+            gb.blur(ip, 5);
+            ip.autoThreshold();
+
+            meansImgs[i+30] = ip.getStatistics().mean;
+            System.out.println(meansImgs[i]);
         }
     }
 }
