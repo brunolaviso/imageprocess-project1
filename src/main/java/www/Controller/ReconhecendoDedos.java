a31c8f8587a39460613e2699936d077270eb0c29
Bruno Laviso <bruno.laviso@hotmail.com>
2020-11-23 22:18:28 -0300

    Adicionada imagens e as classes do trabalho 2 ao projeto

---
+++ src/main/java/www/Controller/ReconhecendoDedos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package www.Controller;
+
+import ij.ImagePlus;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author laviso
+ */
+public class ReconhecendoDedos {
+    
+    // 1 ArrayList 60 posicoes
+    // foreach preenchendo 30 primeiras com 1 dedo
+    // foreach preenchendo 30 ultimas com 3 dedos
+    
+    File[] arquivo;
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 30; i++) {
+            
+            //arquivo[i];
+            
+        }
+    }
+}
